bd221fa2c1eb3391d3b430e24c75b819dfeaeea3
drlcai <dr.leran.cai@gmail.com>
2022-05-15 12:21:50 +0100

    Avellaneda hedging

---
+++ hummingbot/strategy/avellaneda_hedging/__init__.py
@@ -0,0 +1,8 @@
+from .avellaneda_hedging_pair import AvellanedaHedgingPair
+from .avellaneda_hedging import AvellanedaHedgingStrategy
+
+
+__all__ = [
+    AvellanedaHedgingPair,
+    AvellanedaHedgingStrategy,
+]
